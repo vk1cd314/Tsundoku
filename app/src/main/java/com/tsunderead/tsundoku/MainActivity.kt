f7184e731ff690050dab05a765e7a5a64a86dce5
compu <muztoba.sinha@gmail.com>
2022-09-03 23:52:30 +0600

    added fragments of the core app thing

--- app/src/main/java/com/tsunderead/tsundoku/MainActivity.kt
+++ app/src/main/java/com/tsunderead/tsundoku/MainActivity.kt
@@ -2,10 +2,36 @@ package com.tsunderead.tsundoku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.tsunderead.tsundoku.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Library())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.library -> replaceFragment(Library())
+                R.id.updates -> replaceFragment(Updates())
+                R.id.search -> replaceFragment(Search())
+                R.id.history -> replaceFragment(History())
+                R.id.settings -> replaceFragment(Settings())
+                else ->{
+
+                }
+            }
+            true
+        }
+
+    }
+
+    fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }

a82ffb231f17cc72e83761abf99b864053f950cf
vk1cd314 <drakensang47@gmail.com>
2022-09-07 00:21:46 +0600

    Linked all activities and fixed bugs

--- app/src/main/java/com/tsunderead/tsundoku/Settings.kt
+++ app/src/main/java/com/tsunderead/tsundoku/Settings.kt
@@ -1,10 +1,14 @@
 package com.tsunderead.tsundoku
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +20,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Settings.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Settings : Fragment() {
+class Settings : Fragment(R.layout.fragment_settings) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var viewOfLayout: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,9 +38,19 @@ class Settings : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        viewOfLayout = inflater.inflate(R.layout.fragment_settings, container, false)
+        val logout = viewOfLayout.findViewById<TextView>(R.id.log_out)
+        assert(logout != null)
+        logout!!.setOnClickListener {
+            activity?.let {
+                val intent = Intent(it, LoginActivity::class.java)
+                it.startActivity(intent)
+                it.finish()
+            }
+        }
+        return viewOfLayout
     }
 
     companion object {
@@ -57,3 +73,4 @@ class Settings : Fragment() {
             }
     }
 }
+

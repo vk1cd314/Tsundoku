976101c6186e81e21aa0a573d059e9c4607601e0
vk1cd314 <drakensang47@gmail.com>
2022-09-27 16:09:46 +0600

    Can open Manga Detail View but issues with clicks

--- app/src/main/java/com/tsunderead/tsundoku/MangaDetailActivity.kt
+++ app/src/main/java/com/tsunderead/tsundoku/MangaDetailActivity.kt
@@ -1,11 +1,27 @@
 package com.tsunderead.tsundoku
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MangaDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manga_detail)
+        val cover = intent.getIntExtra("Cover", 69)
+        val author = intent.getStringExtra("Author")
+        val title = intent.getStringExtra("Title")
+        println("Cover is $cover")
+        println("Title is $title")
+        println("Author is $author")
+        val authorId = findViewById<TextView>(R.id.author)
+        val titleId = findViewById<TextView>(R.id.title)
+        val coverId = findViewById<ImageView>(R.id.cover)
+        authorId.text = author
+        titleId.text = title
+        coverId.setImageResource(cover)
     }
 }

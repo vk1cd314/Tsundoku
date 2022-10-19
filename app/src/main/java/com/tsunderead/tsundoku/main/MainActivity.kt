8b67a9609b70c6dbcfba9f03b3c08e58af34cb81
vk1cd314 <drakensang47@gmail.com>
2022-10-19 18:31:34 +0600

    Started Navigation Work

--- app/src/main/java/com/tsunderead/tsundoku/main/MainActivity.kt
+++ app/src/main/java/com/tsunderead/tsundoku/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.tsunderead.tsundoku.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -14,40 +15,40 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var library: Fragment = Library()
+    private var updates: Fragment = Updates()
+    private var search: Fragment = Search()
+    private var history: Fragment = History()
+    private var settings: Fragment = Settings()
+    private var activeFragment: Fragment = library
+    private var fragmentManager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        CoroutineScope(Dispatchers.Main).launch {
-            replaceFragment(Library())
-        }
+        fragmentManager.beginTransaction().add(R.id.frame_layout, updates).hide(updates).commit()
+        fragmentManager.beginTransaction().add(R.id.frame_layout, search).hide(search).commit()
+        fragmentManager.beginTransaction().add(R.id.frame_layout, history).hide(history).commit()
+        fragmentManager.beginTransaction().add(R.id.frame_layout, settings).hide(settings).commit()
+        fragmentManager.beginTransaction().add(R.id.frame_layout, library).commit()
+//        replaceFragment(Library())
         binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId) {
                 R.id.library -> {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        replaceFragment(Library())
-                    }
+                    replaceFragment(Library())
                 }
                 R.id.updates -> {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        replaceFragment(Updates())
-                    }
+                    replaceFragment(Updates())
                 }
                 R.id.search -> {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        replaceFragment(Search())
-                    }
+                    replaceFragment(Search())
                 }
                 R.id.history -> {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        replaceFragment(History())
-                    }
+                    replaceFragment(History())
                 }
                 R.id.settings -> {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        replaceFragment(Settings())
-                    }
+                    replaceFragment(Settings())
                 }
 
                 else ->{
@@ -58,15 +59,11 @@ class MainActivity : AppCompatActivity() {
 //        populateLibrary()
     }
 
-    private fun populateLibrary() {
-        val book1 = Manga("https:\\/\\/uploads.mangadex.org\\/covers\\/f9b82990-7198-4131-84bb-c952830f5ea7\\/6754b3ba-a9cd-4f07-89a5-ff4145f24605.jpg", "Nisioisin", "Bakemonogatari", "1")
-        mangaList.add(book1)
-    }
-
     private fun replaceFragment(fragment: Fragment){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, fragment)
-        fragmentTransaction.commit()
+        fragmentManager.beginTransaction().hide(activeFragment).show(fragment).commit()
+        activeFragment = fragment
+        Log.i("wtf", "changed")
+//        fragmentTransaction.replace(R.id.frame_layout, fragment)
+//        fragmentTransaction.commit()
     }
 }

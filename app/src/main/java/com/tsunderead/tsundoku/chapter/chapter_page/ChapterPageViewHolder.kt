a8d3910292d6105fbbb94d91e84cc4e100d32b6e
vk1cd314 <drakensang47@gmail.com>
2022-10-22 04:14:26 +0600

    Material UI Theming + Tried to Fix Image Aspect Ratio, Couldn't

--- app/src/main/java/com/tsunderead/tsundoku/chapter/chapter_page/ChapterPageViewHolder.kt
+++ app/src/main/java/com/tsunderead/tsundoku/chapter/chapter_page/ChapterPageViewHolder.kt
@@ -1,5 +1,7 @@
 package com.tsunderead.tsundoku.chapter.chapter_page
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.util.Log
 import android.view.MotionEvent
 import androidx.core.view.ViewCompat.canScrollHorizontally
 import androidx.recyclerview.widget.RecyclerView
@@ -7,6 +9,7 @@ import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.tsunderead.tsundoku.R
 import com.tsunderead.tsundoku.databinding.ChapterImageViewBinding
+import kotlinx.coroutines.*
 
 class ChapterPageViewHolder(private val chapterPageBinding: ChapterImageViewBinding)
     : RecyclerView.ViewHolder(chapterPageBinding.root){
@@ -14,6 +17,12 @@ class ChapterPageViewHolder(private val chapterPageBinding: ChapterImageViewBind
     @SuppressLint("ClickableViewAccessibility")
     fun bindChapter(chapterPage: ChapterPage, chapterPageViewHolder: ChapterPageViewHolder) {
         val imgUrl = chapterPage.chapterPageId
-        Glide.with(chapterPageViewHolder.itemView.context).load(imgUrl).placeholder(R.drawable.placeholder).into(chapterPageBinding.chapterPageImageView)
+        val bitmap = Glide.with(chapterPageViewHolder.itemView.context).load(imgUrl).placeholder(R.drawable.placeholder).into(chapterPageBinding.chapterPageImageView)
+        bitmap.getSize { width, height ->
+            Log.i("BitMap Info", "$width and $height")
+            Log.i("Image Info", chapterPageBinding.chapterPageImageView.layoutParams.width.toString() + " and " + chapterPageBinding.chapterPageImageView.layoutParams.height.toString())
+            chapterPageBinding.chapterPageImageView.layoutParams.width = width
+            chapterPageBinding.chapterPageImageView.layoutParams.height = height
+        }
     }
 }

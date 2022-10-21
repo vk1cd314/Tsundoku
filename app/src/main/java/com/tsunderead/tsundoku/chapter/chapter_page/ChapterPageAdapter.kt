e57e911ae5079c686654b2e084478b27781f0eb4
vk1cd314 <drakensang47@gmail.com>
2022-10-22 00:36:03 +0600

    Image Zooming Works

--- app/src/main/java/com/tsunderead/tsundoku/chapter/chapter_page/ChapterPageAdapter.kt
+++ app/src/main/java/com/tsunderead/tsundoku/chapter/chapter_page/ChapterPageAdapter.kt
@@ -1,26 +1,46 @@
 package com.tsunderead.tsundoku.chapter.chapter_page
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import androidx.recyclerview.widget.RecyclerView
 import com.tsunderead.tsundoku.databinding.ChapterImageViewBinding
 
 class ChapterPageAdapter (private val chapterPages: ArrayList<ChapterPage>)
     : RecyclerView.Adapter<ChapterPageViewHolder>() {
     override fun getItemCount(): Int {
-//        TODO("Not yet implemented")
         return chapterPages.size
     }
 
     override fun onBindViewHolder(holder: ChapterPageViewHolder, position: Int) {
-//        TODO("Not yet implemented")
         holder.bindChapter(chapterPages[position], holder)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChapterPageViewHolder {
-//        TODO("Not yet implemented")
         val from = LayoutInflater.from(parent.context)
         val binding = ChapterImageViewBinding.inflate(from, parent, false)
+        binding.chapterPageImageView.apply {
+            setOnTouchListener { view, event ->
+                var result = true
+                if (event.pointerCount >= 2 || view.canScrollHorizontally(1) && canScrollHorizontally(-1)) {
+                    result = when (event.action) {
+                        MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                            parent.requestDisallowInterceptTouchEvent(true)
+                            false
+                        }
+                        MotionEvent.ACTION_UP -> {
+                            parent.requestDisallowInterceptTouchEvent(false)
+                            true
+                        }
+                        else -> true
+                    }
+                }
+                result
+            }
+        }
         return ChapterPageViewHolder(binding)
     }
 

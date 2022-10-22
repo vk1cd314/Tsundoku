92c66cd10df79470a7ec99b55f5070accd912e84
master-da <mdnokib2000@gmail.com>
2022-10-22 20:47:30 +0600

    looks a lil nicer. unique username and email. signup allows username and email.

--- app/src/main/java/com/tsunderead/tsundoku/account/CreateAccountActivity.kt
+++ app/src/main/java/com/tsunderead/tsundoku/account/CreateAccountActivity.kt
@@ -3,12 +3,14 @@ package com.tsunderead.tsundoku.account
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.tsunderead.tsundoku.R
 
@@ -16,6 +18,7 @@ class CreateAccountActivity : AppCompatActivity() {
 
     private val tag = "CreateAccountActivity"
     private lateinit var auth: FirebaseAuth
+    private val db = Firebase.firestore
 
     private lateinit var username: TextInputEditText
     private lateinit var email: TextInputEditText
@@ -38,23 +41,64 @@ class CreateAccountActivity : AppCompatActivity() {
         }
     }
 
-    private fun badAccountHandling() : Boolean {
+    private fun badAccount() : Boolean {
         var errorText = ""
         if (username.text.isNullOrEmpty()) errorText = "Please enter username"
         else if (email.text.isNullOrEmpty()) errorText = "Please enter email"
+        else if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) errorText = "Invalid email"
         else if (password.text.isNullOrEmpty()) errorText = "Please enter password"
         else if (password.text.toString() != confirmPassword.text.toString()) errorText = "Passwords Don't match"
-        Log.i(tag, password.text.toString())
-        Log.i(tag, confirmPassword.text.toString())
-        Log.e(tag, (password.text != confirmPassword.text).toString())
+
         if (errorText.isNotEmpty()) Toast.makeText(baseContext, errorText, Toast.LENGTH_LONG).show()
         return errorText.isNotEmpty()
     }
 
+    private fun createAccountIfUnique() {
+        var uniqueEmail = false
+        var uniqueUsername = false
+        db.collection("account").whereEqualTo("email", email.text.toString()).get()
+            .addOnSuccessListener {
+                if (it.size() > 0) Toast.makeText(baseContext, "Email Already Registered", Toast.LENGTH_LONG).show()
+                else {
+                    uniqueEmail = true
+                    // TODO change next line to if (uniqueEmail = true && uniqueUsername) to make it look cooler. don't wanna risk it now
+                    if (uniqueEmail && uniqueUsername)
+                        firebaseAuth()
+                }
+            }
+        db.collection("account").whereEqualTo("username", username.text.toString()).get()
+            .addOnSuccessListener {
+                if (it.size() > 0) Toast.makeText(baseContext, "Username Already Registered", Toast.LENGTH_LONG).show()
+                else {
+                    uniqueUsername = true
+                    if (uniqueEmail && uniqueUsername)
+                        firebaseAuth()
+                }
+            }
+    }
+
     private fun createAccount () {
-        if (badAccountHandling()) return
+        if (badAccount()) return
+        createAccountIfUnique()
+    }
+
+    private fun firebaseAuth() {
+        val user = hashMapOf(
+            "username" to username.text.toString(),
+            "email" to email.text.toString(),
+            "password" to password.text.toString()
+        )
         auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener(this) {
             if (it.isSuccessful) {
+
+                db.collection("account").add(user)
+                    .addOnSuccessListener {
+                        Log.i(tag, "user with info created")
+                    }
+                    .addOnFailureListener{
+                        Log.e(tag, "user addition failed. user: , $user")
+                    }
+
                 val intent = Intent(this, LoginActivity::class.java)
                 intent.putExtra("email", email.text.toString())
                 intent.putExtra("password", password.text.toString())
@@ -63,7 +107,6 @@ class CreateAccountActivity : AppCompatActivity() {
             }
             else {
                 Log.e(tag, it.exception.toString())
-                Toast.makeText(baseContext, "Please enter a valid email address", Toast.LENGTH_SHORT).show()
             }
         }
     }
--- app/src/main/java/com/tsunderead/tsundoku/account/LoginActivity.kt
+++ app/src/main/java/com/tsunderead/tsundoku/account/LoginActivity.kt
@@ -3,12 +3,13 @@ package com.tsunderead.tsundoku.account
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.tsunderead.tsundoku.R
 import com.tsunderead.tsundoku.main.MainActivity
@@ -16,7 +17,10 @@ import com.tsunderead.tsundoku.main.MainActivity
 class LoginActivity : AppCompatActivity() {
 
     private val TAG = "LoginActivity"
-    private lateinit var auth: FirebaseAuth
+
+    private val db = Firebase.firestore
+    private val auth = Firebase.auth
+
     private lateinit var email: TextInputEditText
     private lateinit var password: TextInputEditText
 
@@ -24,12 +28,8 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        auth = Firebase.auth
         email = findViewById(R.id.loginEmail)
         password = findViewById(R.id.loginPassword)
-        val loginButton = findViewById<Button>(R.id.loginButton)
-        val signupButton = findViewById<Button>(R.id.signup_button)
-        val forgotPassword = findViewById<Button>(R.id.forgot_password_button)
 
         val extras = intent.extras
         if (extras != null) {
@@ -37,28 +37,71 @@ class LoginActivity : AppCompatActivity() {
             password.setText(extras.getString("password"))
         }
 
-        loginButton.setOnClickListener {
-            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener(this) {
-                if (it.isSuccessful) {
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                }else {
-                    Log.w(TAG, "Could not sign in", it.exception)
-                    Toast.makeText(baseContext, "Sign In Failed: No Matching email or password", Toast.LENGTH_SHORT).show()
-                }
+        initSignup ()
+        initForgotPass()
+        initSignIn()
 
-            }
-        }
+    }
+    private fun initSignup () {
+        val signupButton = findViewById<Button>(R.id.signup_button)
         signupButton.setOnClickListener {
             val intent = Intent(this, CreateAccountActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun initForgotPass () {
+        val forgotPassword = findViewById<Button>(R.id.forgot_password_button)
         forgotPassword.setOnClickListener {
             val intent = Intent(this, ForgotPasswordActivity::class.java)
             startActivity(intent)
             finish()
+        }
+    }
+
+    private fun initSignIn () {
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            login(email.text.toString(), password.text.toString())
+        }
+    }
+
+    private fun badLogin(): Boolean {
+        var error = "";
+        if (email.text.isNullOrEmpty()) error = "Please enter username or email"
+        else if (password.text.isNullOrEmpty()) error = "Please enter Password"
+        if (error.isNotEmpty()) Toast.makeText(baseContext, error, Toast.LENGTH_SHORT).show()
+        return error.isNotEmpty()
+    }
+
+    private fun login (loginName: String, loginPass: String) {
+        if (badLogin()) return
+        if (!Patterns.EMAIL_ADDRESS.matcher(loginName).matches()) {
+            db.collection("account").whereEqualTo("username", loginName).get()
+                .addOnSuccessListener {
+                    if (it.size() > 0)
+                        firebaseAuth(it.documents[0].data?.get("email").toString(), loginPass)
+                    else Toast.makeText(baseContext, "No Username Found", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener{
+                    Log.e(TAG, "failed", it)
+                }
+        }
+        else firebaseAuth(loginName, loginPass)
+    }
+
+    private fun firebaseAuth (loginMail: String, loginPass: String) {
+        auth.signInWithEmailAndPassword(loginMail, loginPass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else {
+                Log.w(TAG, "Could not sign in", it.exception)
+                Toast.makeText(baseContext, "Sign In Failed: No Matching email or password", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }

3653805d393456f0d10b9fcfa556965a3527275e
vk1cd314 <drakensang47@gmail.com>
2022-10-22 23:52:36 +0600

    Can Resume Chapter

--- app/src/main/java/com/tsunderead/tsundoku/history/HistoryChapterViewHolder.kt
+++ app/src/main/java/com/tsunderead/tsundoku/history/HistoryChapterViewHolder.kt
@@ -1,10 +1,12 @@
 package com.tsunderead.tsundoku.history
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.tsunderead.tsundoku.R
 import com.tsunderead.tsundoku.databinding.HistoryCellBinding
+import com.tsunderead.tsundoku.manga_reader.MangaReaderActivity
 
 class HistoryChapterViewHolder(private val historyCellBinding: HistoryCellBinding)
     : RecyclerView.ViewHolder(historyCellBinding.root){
@@ -14,5 +16,10 @@ class HistoryChapterViewHolder(private val historyCellBinding: HistoryCellBindin
         Glide.with(historyChapterViewHolder.itemView.context).load(imgUrl).placeholder(R.drawable.placeholder).into(historyCellBinding.mangaHistoryImageView)
         historyCellBinding.mangaNameTextView.text = mangaWithChapter.manga.title
         historyCellBinding.chapterIDTextView.text = "Chapter ${mangaWithChapter.chapter.chapterNumber}"
+        historyCellBinding.resumeChapterButton.setOnClickListener {
+            val intent = Intent(it.context, MangaReaderActivity::class.java)
+            intent.putExtra("ChapterId", mangaWithChapter.chapter.chapterHash)
+            it.context.startActivity(intent)
+        }
     }
 }

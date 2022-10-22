1990bf4682b69ba4fc1158578c49d45c7ab4e314
master-da <mdnokib2000@gmail.com>
2022-10-22 16:29:27 +0600

    sign up and login works looks ugly

--- app/src/main/java/com/tsunderead/tsundoku/account/CreateAccountActivity.kt
+++ app/src/main/java/com/tsunderead/tsundoku/account/CreateAccountActivity.kt
@@ -2,19 +2,70 @@ package com.tsunderead.tsundoku.account
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.tsunderead.tsundoku.R
 
 class CreateAccountActivity : AppCompatActivity() {
+
+    private val tag = "CreateAccountActivity"
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var username: TextInputEditText
+    private lateinit var email: TextInputEditText
+    private lateinit var password: TextInputEditText
+    private lateinit var confirmPassword: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_account)
+
+        auth = Firebase.auth
+        username = findViewById(R.id.signupUsername)
+        email = findViewById(R.id.signupEmail)
+        password = findViewById(R.id.signupPassword)
+        confirmPassword = findViewById(R.id.signupConfirmPassword)
+
         val signup = findViewById<Button>(R.id.signup)
         signup.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+            createAccount()
         }
     }
+
+    private fun badAccountHandling() : Boolean {
+        var errorText = ""
+        if (username.text.isNullOrEmpty()) errorText = "Please enter username"
+        else if (email.text.isNullOrEmpty()) errorText = "Please enter email"
+        else if (password.text.isNullOrEmpty()) errorText = "Please enter password"
+        else if (password.text.toString() != confirmPassword.text.toString()) errorText = "Passwords Don't match"
+        Log.i(tag, password.text.toString())
+        Log.i(tag, confirmPassword.text.toString())
+        Log.e(tag, (password.text != confirmPassword.text).toString())
+        if (errorText.isNotEmpty()) Toast.makeText(baseContext, errorText, Toast.LENGTH_LONG).show()
+        return errorText.isNotEmpty()
+    }
+
+    private fun createAccount () {
+        if (badAccountHandling()) return
+        auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.putExtra("email", email.text.toString())
+                intent.putExtra("password", password.text.toString())
+                startActivity(intent)
+                finish()
+            }
+            else {
+                Log.e(tag, it.exception.toString())
+                Toast.makeText(baseContext, "Please enter a valid email address", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/tsunderead/tsundoku/account/LoginActivity.kt
+++ app/src/main/java/com/tsunderead/tsundoku/account/LoginActivity.kt
@@ -2,28 +2,59 @@ package com.tsunderead.tsundoku.account
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.tsunderead.tsundoku.R
 import com.tsunderead.tsundoku.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private val TAG = "LoginActivity"
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email: TextInputEditText
+    private lateinit var password: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-        val loginButton = findViewById<Button>(R.id.login_button)
-        loginButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+
+        auth = Firebase.auth
+        email = findViewById(R.id.loginEmail)
+        password = findViewById(R.id.loginPassword)
+        val loginButton = findViewById<Button>(R.id.loginButton)
         val signupButton = findViewById<Button>(R.id.signup_button)
+        val forgotPassword = findViewById<Button>(R.id.forgot_password_button)
+
+        val extras = intent.extras
+        if (extras != null) {
+            email.setText(extras.getString("email"))
+            password.setText(extras.getString("password"))
+        }
+
+        loginButton.setOnClickListener {
+            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }else {
+                    Log.w(TAG, "Could not sign in", it.exception)
+                    Toast.makeText(baseContext, "Sign In Failed: No Matching email or password", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
         signupButton.setOnClickListener {
             val intent = Intent(this, CreateAccountActivity::class.java)
             startActivity(intent)
             finish()
         }
-        val forgotPassword = findViewById<Button>(R.id.forgot_password_button)
         forgotPassword.setOnClickListener {
             val intent = Intent(this, ForgotPasswordActivity::class.java)
             startActivity(intent)

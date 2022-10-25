1bdeb5d88376a4eb062578da7de2355ff0aa7913
master-da <mdnokib2000@gmail.com>
2022-10-25 20:59:09 +0600

    manga tags and description added

--- app/src/main/java/com/tsunderead/tsundoku/NewPost.kt
+++ app/src/main/java/com/tsunderead/tsundoku/NewPost.kt
@@ -1,45 +1,39 @@
 package com.tsunderead.tsundoku
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import com.tsunderead.tsundoku.databinding.FragmentCommunityBinding
+import com.tsunderead.tsundoku.databinding.FragmentNewPostBinding
+import com.tsunderead.tsundoku.databinding.FragmentSearchBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class NewPost : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class NewPost : DialogFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentNewPostBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_post, container, false)
+        binding = FragmentNewPostBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NewPost().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        return dialog
     }
 }
--- app/src/main/java/com/tsunderead/tsundoku/main/Community.kt
+++ app/src/main/java/com/tsunderead/tsundoku/main/Community.kt
@@ -2,13 +2,14 @@ package com.tsunderead.tsundoku.main
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.tsunderead.tsundoku.R
+import androidx.fragment.app.FragmentTransaction
+import com.tsunderead.tsundoku.NewPost
 import com.tsunderead.tsundoku.databinding.FragmentCommunityBinding
-import com.tsunderead.tsundoku.helperclasses.OnSwipeTouchListener
 
 class Community : Fragment(){
 
@@ -21,14 +22,17 @@ class Community : Fragment(){
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentCommunityBinding.inflate(inflater, container, false)
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.btnCommunityAdd.setOnClickListener{
-
+            NewPost().show(childFragmentManager, "New Post")
+//            val fragmentManger =  childFragmentManager
+//            val transaction = fragmentManger.beginTransaction()
+//            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+//            transaction.add(android.R.id.content, NewPost()).addToBackStack(null).commit()
         }
 
     }

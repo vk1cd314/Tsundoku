fe5a54990c702cd4c67725757884bed566bfdb34
vk1cd314 <drakensang47@gmail.com>
2022-10-26 01:23:29 +0600

    THE MANGA READER ACTUALLY WORKS FOR ONCE

--- app/src/main/java/com/tsunderead/tsundoku/chapter/chapter_page/ChapterPageViewHolder.kt
+++ app/src/main/java/com/tsunderead/tsundoku/chapter/chapter_page/ChapterPageViewHolder.kt
@@ -7,6 +7,8 @@ import androidx.core.view.ViewCompat.canScrollHorizontally
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.request.target.SimpleTarget
 import com.tsunderead.tsundoku.R
 import com.tsunderead.tsundoku.databinding.ChapterImageViewBinding
 import kotlinx.coroutines.*
@@ -17,12 +19,6 @@ class ChapterPageViewHolder(private val chapterPageBinding: ChapterImageViewBind
     @SuppressLint("ClickableViewAccessibility")
     fun bindChapter(chapterPage: ChapterPage, chapterPageViewHolder: ChapterPageViewHolder) {
         val imgUrl = chapterPage.chapterPageId
-        val bitmap = Glide.with(chapterPageViewHolder.itemView.context).load(imgUrl).placeholder(R.drawable.placeholder).into(chapterPageBinding.chapterPageImageView)
-        bitmap.getSize { width, height ->
-            Log.i("BitMap Info", "$width and $height")
-            Log.i("Image Info", chapterPageBinding.chapterPageImageView.layoutParams.width.toString() + " and " + chapterPageBinding.chapterPageImageView.layoutParams.height.toString())
-            chapterPageBinding.chapterPageImageView.layoutParams.width = width
-            chapterPageBinding.chapterPageImageView.layoutParams.height = height
-        }
+        Glide.with(chapterPageViewHolder.itemView.context).download(GlideUrl(imgUrl)).into(SubsamplingScaleImageViewTarget(chapterPageBinding.chapterPageImageView))
     }
 }

999a3d2c8aec6432610bf7b684c51f8989b8a9e1
vk1cd314 <drakensang47@gmail.com>
2022-10-26 21:39:19 +0600

    Wasted a bunch of time on nothing

--- app/src/main/java/com/tsunderead/tsundoku/main/Library.kt
+++ app/src/main/java/com/tsunderead/tsundoku/main/Library.kt
@@ -2,10 +2,16 @@ package com.tsunderead.tsundoku.main
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.view.MenuItem.OnMenuItemClickListener
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.tsunderead.tsundoku.R
@@ -13,6 +19,8 @@ import com.tsunderead.tsundoku.databinding.FragmentLibraryBinding
 import com.tsunderead.tsundoku.manga_card_cell.CardCellAdapter
 import com.tsunderead.tsundoku.manga_card_cell.Manga
 import com.tsunderead.tsundoku.offlinedb.LibraryDBHelper
+import java.util.*
+import kotlin.collections.ArrayList
 
 class Library : Fragment() {
     private var fragmentLibraryBinding: FragmentLibraryBinding? = null
@@ -21,7 +29,6 @@ class Library : Fragment() {
     private lateinit var recyclerView : RecyclerView
 
     private lateinit var mangaList : ArrayList<Manga>
-    private lateinit var covers : Array<Int>
 
     private lateinit var libraryDBHandler : LibraryDBHelper
 
@@ -41,22 +48,51 @@ class Library : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+//        (activity as AppCompatActivity?)!!.setSupportActionBar(fragmentLibraryBinding?.libraryToolbar as Toolbar?)
         fragmentLibraryBinding?.libraryToolbar?.inflateMenu(R.menu.library_toolbar_menu)
         fragmentLibraryBinding?.libraryToolbar?.title = "Library"
         fragmentLibraryBinding?.libraryToolbar?.setOnMenuItemClickListener {
             when(it.itemId){
                 R.id.library_search -> {
+                    Log.i("Inside", "Lib search")
+                    val searchButton = it.actionView as SearchView
+                    searchButton.queryHint = "Search"
+                    searchButton.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                        override fun onQueryTextSubmit(query: String?): Boolean {
+                            return false
+                        }
+                        override fun onQueryTextChange(newText: String?): Boolean {
+                            // filter here
+                            if (newText != null) {
+                                filter(newText)
+                            }
+                            return false
+                        }
+                    })
                     true
                 }
                 else -> false
             }
         }
+        setRecyclerViewAdapter()
         dataInit()
     }
+
 
     override fun onResume() {
         super.onResume()
         dataInit()
+    }
+
+    private fun filter(query: String) {
+        Log.i("Filtering", query)
+        val filteredList: ArrayList<Manga> = arrayListOf()
+        for (item in mangaList) {
+            if (item.title.lowercase().contains(query.lowercase())) {
+                filteredList.add(item)
+            }
+        }
+        adapter.changeList(filteredList)
     }
 
     @SuppressLint("Range", "UseRequireInsteadOfGet")
@@ -78,13 +114,16 @@ class Library : Fragment() {
             cursor.moveToNext()
         }
         libraryDBHandler.close()
+        adapter.changeList(mangaList)
+    }
+
+    private fun setRecyclerViewAdapter() {
         val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         recyclerView = fragmentLibraryBinding?.libraryRecylerView ?: recyclerView
         recyclerView.layoutManager = layoutManager
         recyclerView.setHasFixedSize(true)
+        mangaList = arrayListOf()
         adapter = CardCellAdapter(mangaList)
         recyclerView.adapter = adapter
-
-
     }
 }
